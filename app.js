ea66dfbe5e9de6d4cd6f290e3bd8c03da31d1a12
Rafa Elias <rafaeliasuzcategui@gmail.com>
2019-12-16 00:21:10 -0500

    Models and contollers created

--- app.js
+++ app.js
@@ -1,16 +1,13 @@
 const express = require("express")
 const app = express()
 const router = require("./router")
-console.log(router)
 
+
+app.use(express.urlencoded({extended: false}))
+app.use(express.json())
 app.use(express.static(__dirname + "/public"))
 app.set("views", "views")
 app.set("view engine", "ejs")
+app.use("/", router)
 
-app.get("/", function(req, res){
-    res.render("home-guest")
-})
-
-app.listen(3000, function(){
-    console.log("listen port 3000")
-})
+module.exports = app
--- router.js
+++ router.js
@@ -1,3 +1,13 @@
-console.log("dddddd")
+const express = require("express")
+const router = express.Router()
+const userController = require("./controllers/userController")
 
-module.exports = "fkfkfkkfk"
+router.get("/", userController.home)
+
+router.post("/register", userController.register)
+
+router.post("/login", userController.login)
+
+
+
+module.exports = router
