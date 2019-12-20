cfc207dff4bf27702c7d1d6e5b1ffffe8b8992a0
Rafa Elias <rafaeliasuzcategui@gmail.com>
2019-12-19 23:16:33 -0500

    Sessions, Flash, login and logout added

--- models/User.js
+++ models/User.js
@@ -1,4 +1,5 @@
-const usersCollection = require("../db").collection("users")
+const bcrypt = require("bcryptjs");
+const usersCollection = require("../db").db().collection("users")
 const validator = require("validator")
 let  User = function(data){
     this.data = data
@@ -18,22 +19,34 @@ User.prototype.cleanUp = function(){
     }
 }
 
-User.prototype.validate = function(){
+User.prototype.validate = async function(){
     if(this.data.username == "") {this.errors.push("You must provide a username")}
     if(this.data.username != "" && !validator.isAlphanumeric(this.data.username)){this.errors.push("Username can only contain letters and numbers. ")}
     if(!validator.isEmail(this.data.email)) {this.errors.push("You must provide a valid email address")}
     if(this.data.password == "") {this.errors.push("You must provide a password")}
     if(this.data.password.length > 0 && this.data.password.length < 12){this.errors.push("Password must be at least 12 characters")}
-    if(this.data.password.length > 100){this.errors.push("Password cannot exceed 100 characters")}
+    if(this.data.password.length > 50){this.errors.push("Password cannot exceed 50 characters")}
     if(this.data.username.length > 0 && this.data.username.length < 3){this.errors.push("Username must be at least 3 characters")}
     if(this.data.username.length > 30){this.errors.push("Username cannot exceed 30 characters")}
+
+    //Only if username is valid then check to see if it's already taken
+    if(this.data.username.length > 2 && this.data.username.length <31 &&  validator.isAlphanumeric(this.data.username)){
+        let usernameExists = await usersCollection.findOne({username: this.data.username})
+        if(usernameExists){this.errors.push("That username is already taken.")}
+    }
+
+    //Only if email is valid then check to see if it's already taken
+    if(validator.isEmail(this.data.email)){
+        let emailExists = await usersCollection.findOne({email: this.data.email})
+        if(emailExists){this.errors.push("That email is already being used.")}
+    }
 }
 
 User.prototype.login = function(){
     return new Promise((resolve, reject) =>{
         this.cleanUp()
     usersCollection.findOne({username: this.data.username}).then((attemptedUser)=>{
-        if(attemptedUser && attemptedUser.password == this.data.password){
+        if(attemptedUser && bcrypt.compareSync(this.data.password, attemptedUser.password)){
             resolve("congrats")
         }else{
             reject("Invalid username/password")
@@ -51,6 +64,9 @@ User.prototype.register = function(){
     // Step #2: Only if there are no validation errors 
     // then save the user data into database
     if(!this.errors.length){
+        //Hash user password
+        let salt = bcrypt.genSaltSync(10)
+        this.data.password = bcrypt.hashSync(this.data.password, salt)
         usersCollection.insertOne(this.data)
     }
 }
